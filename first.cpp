0426a64c7857f4cec29185e599105c1a0da50310
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-08-14 23:44:44 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/c++io.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int> g={1,2,3,4,5};
+
+        for(auto &it :g){
+        cout<<"The value of g in vector is: ",it<<endl;
+    }
+    return 0;
+}
