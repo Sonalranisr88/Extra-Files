de405152c5b1514ab06550af520643523e5e99a3
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-08-15 23:36:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello world");
+ 
+// var a= 10;
+// var b =7;
+// var sum =a+b;
+// var diff=a-b;
+// var product=a*b;
+// var divide=a/b;
+// var rem=a%b;
+
+
+// console.log("sum: ",sum);
+// console.log("diff: ",diff);
+// console.log("product: ",product);
+// console.log("divide: ",divide);
+// console.log("rem: ",rem);
+
+var a=1;
+while
+    (a<=10){
+    console.log(a)
+    a++;
+}
